2f420893ab5ce550f75fd273cad7bdfc932de18b
rajchaudar <rajchaudar60@gmail.com>
2025-04-05 18:23:59 +0530

    Update API_BASE_URL to point to the correct production endpoint

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
     ? "http://localhost:3000/api"
-    : "https://hr-dep-1.onrender.com/api";
+    : "https://hr-copy.onrender.com/api";
 
 
 // const stripe = Stripe("pk_test_51PktNASFRtv588FKNJTtUvfMUXEnkFK1aGeRIdBT9PxICcVxYb99NtuKxpMZxGSFy8YfOU2SmceVspuVXLhBjeia00W4KD5BVW"); 
